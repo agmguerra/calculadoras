8335553b81febbb9275a68bb98d558ac9df4466d
AlexandreGuerra <agmguerra@gmail.com>
2020-02-23 14:06:22 -0300

    ajuste

--- calcfunctions.js
+++ calcfunctions.js
@@ -1,10 +1,20 @@
 
 // Converter uma taxa mensal para anual
 function getTxAnualFromTxMensal(txMensal) {
-  return Math.pow((1 + txMensal), 12) - 1;
+  const res = (Math.pow((1 + (txMensal/100)), 12) - 1) * 100;
+  console.log('Anual = ' + res);
+  
+  return res;
 }
 
 // Converter uma taxa anual para mensal
-function getTxMensalFromTxAnual(params) {
-  return Math.pow(1 + txMensal), (1/12) - 1;
+function getTxMensalFromTxAnual(txAnual) {
+  const res = (Math.pow((1 + (txAnual/100)), (1/12)) - 1) * 100;
+  console.log('Mensal = ' + res);
+  return res;
+}
+
+function getTxConvertida(periodoAtual, txAtual, periodoDesejado) {
+  const tx =  (Math.pow((1 + (txAtual/100)), (periodoDesejado / periodoAtual)) - 1) * 100;
+  return tx;
 }
